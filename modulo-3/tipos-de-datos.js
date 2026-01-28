47bc653131386db52aa2e1e9860e15aba0465f68
Jorge Mares <maresj1411@gmail.com>
2026-01-27 20:18:47 -0600

    nuevo modulo

---
+++ modulo-3/tipos-de-datos.js
@@ -0,0 +1,26 @@
+//Tipos de datos 
+//Strings
+/* Tambien se puede comentar asi en JS */
+"Hola soy un String"
+'Hola yo tambien soy un string'
+//Numbers
+2;
+1.1; -
+1;
+//tipo de dato booleano 
+true; //VERDADERO    
+
+false; //FALSO   
+//Null
+null
+//Undefined
+undefined
+
+console.log(typeof "Esto es un String");
+console.log(typeof 143);
+console.log(typeof -143);
+console.log(typeof "");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
